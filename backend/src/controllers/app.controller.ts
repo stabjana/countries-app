4cd753e53ffe33692aa5eb34270341763774750d
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-07 12:17:09 +0200

    cloned repo from martin_holland and updated

--- backend/src/controllers/app.controller.ts
+++ backend/src/controllers/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../services/app.service';
 
-@Controller()
+@Controller() // extends the controller with AppService?
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // get request to forward slash - /
   getHello(): string {
     return this.appService.getHello();
   }
