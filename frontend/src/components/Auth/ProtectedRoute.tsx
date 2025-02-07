06a1cdfffa794b894aedf40a1edde1fd420e5075
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-07 14:45:09 +0200

    add PRotectedRoute component

---
+++ frontend/src/components/Auth/ProtectedRoute.tsx
@@ -0,0 +1,35 @@
+// we pass through the children to login
+
+import { Box, CircularProgress } from "@mui/material";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <>{children}</>;
+};
