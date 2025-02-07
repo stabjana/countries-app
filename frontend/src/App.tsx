a9f079369ba03cfa07f138d978be5ddef23cbe6c
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-07 14:58:50 +0200

    add Navigation to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,14 @@ import { TestData } from "./components/TestData";
 import { Login } from "./components/Auth/Login";
 import { ProtectedRoute } from "./components/Auth/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
+import { Navigation } from "./components/Auth/Navigation";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Box>
-    
+          <Navigation />
           <Box sx={{ p: 3 }}>
             <Routes>
               <Route path="/login" element={<Login />} />
@@ -19,7 +20,7 @@ function App() {
                 path="/protected"
                 element={
                   <ProtectedRoute>
-                    < > </>
+                    <> </>
                   </ProtectedRoute>
                 }
               />
