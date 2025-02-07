d68cc4c126f31e58ef896e03e6e2141f81c1e9d5
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-07 14:57:42 +0200

    add Navigation with login logic

---
+++ frontend/src/components/Auth/Navigation.tsx
@@ -0,0 +1,25 @@
+import { AppBar, Button, Toolbar } from "@mui/material";
+import { useAuth } from "../../context/AuthContext";
+import {Link as RouterLink} from "react-router-dom";
+
+export const Navigation = () => {
+  const { user, signOut } = useAuth();
+
+  return (
+    <AppBar position="static" sx={{ mb: 3 }}>
+      <Toolbar>
+        <Button color="inherit" component={RouterLink} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={RouterLink} to="/test">
+          Test
+        </Button>
+        {user ? (
+            <Button color="inherit" onClick={signOut}>Logout</Button>
+        ) : (
+            <Button color="inherit" component = {RouterLink} to="/login">Login</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
