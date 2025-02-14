5264c0b7550dc870732c2ad7ddefcf9d8bbbb57b
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-14 14:55:31 +0200

    add cases to create the slice

--- frontend/src/store/slices/countriesSlices.ts
+++ frontend/src/store/slices/countriesSlices.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { CountryState } from "../../types/country";
+import { countriesApi } from "../../api/services/countries";
 
 const initialState: CountryState = {
     countries: [],
@@ -9,7 +10,8 @@ const initialState: CountryState = {
 }
 
 export const fetchAlLCountries = createAsyncThunk('countries/fetchAllCountries', async () => {
-    // todo
+    const response = await countriesApi.getAllCountries();
+    return response;
 })
 
 export const countriesSlice = createSlice ({
@@ -22,7 +24,19 @@ export const countriesSlice = createSlice ({
         }
     },
     extraReducers: (builder) => {
-     // funct
+     builder.addCase(fetchAlLCountries.pending, (state)=> {
+        state.loading = true;
+     })
+     .addCase(fetchAlLCountries.fulfilled, (state, action) => {
+        state.loading =false;
+        state.countries = action.payload;
+     })
+     .addCase(fetchAlLCountries.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string || 'Failed to load countries'
+        // we force a type change here in case of the thing is rejected
+        // we dont know what we get back, we tell it interpret it as a string
+     })
     }
 
 })
