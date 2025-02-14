c9c583f04e318c0195c9bcff8955e62848472fb1
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-14 14:37:08 +0200

    add country.ts with interfaces in types

---
+++ frontend/src/store/slices/countriesSlices.ts
@@ -0,0 +1,9 @@
+import { CountryState } from "../../types/country";
+
+const initialState: CountryState = {
+    countries: [],
+    loading: false,
+    error: null,
+    selectedCountry: null,
+}
+
---
+++ frontend/src/types/country.ts
@@ -0,0 +1,34 @@
+export interface CountryName {
+  common: string;
+  official: string;
+  nativeName: Record<string, { official: string; common: string }>;
+}
+
+export interface CountryFlags {
+  png: string;
+  svg: string;
+  alt?: string;
+}
+
+export interface Currency {
+  name: string;
+  symbol: string;
+}
+
+export interface Country {
+    name: CountryName;
+    capital?: string[];
+    region: string;
+    subregion?: string;
+    population: number;
+    flags: CountryFlags;
+    cca3: string;
+    currencies?: Record<string, Currency>;
+}
+
+export interface CountryState {
+    countries: Country[];
+    loading: boolean;
+    error: string | null;
+    selectedCountry: Country | null;
+}
