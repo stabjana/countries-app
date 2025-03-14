0e24110246c509314de31c14e0c54eca84b12aec
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-14 13:54:22 +0200

    add cypress framework and setup files

---
+++ frontend/cypress.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:5180",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      return config;
+    },
+  },
+});
